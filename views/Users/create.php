a02aa19688950d0786da35697df092f2cf4269ea
sohail042414 <sohail042414@gmail.com>
2024-12-16 19:37:10 +0500

    extra code deleted

--- views/Users/create.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\helpers\Html;
-
-$this->title = 'Create User';
-?>
-<h1><?= Html::encode($this->title) ?></h1>
-
-<?= $this->render('_form', ['model' => $model]) ?>
--- views/Users/update.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\helpers\Html;
-
-$this->title = 'Update User: ' . $model->username;
-?>
-<h1><?= Html::encode($this->title) ?></h1>
-
-<?= $this->render('_form', ['model' => $model]) ?>
